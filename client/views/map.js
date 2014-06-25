494d0dc9f1bda385708dedfc8faae4a6bf4b6c18
Rein Appeldoorn <reinzor@gmail.com>
2014-06-25 22:40:00 +0200

    Updated content

--- client/views/map.js
+++ client/views/map.js
@@ -88,20 +88,42 @@ clearLocations = function() {
     });
 }
 
-setupLocations = function() {
+setupTest = function() {
+    clearLocations();
+
     Points.insert({
         "name":"Speeltuintje Kuiper",
         "location":{
             "latitude": 51.412034,
             "longitude": 6.032487
         },
-        "content" : "<ul>
-        <li>Gefeliciteerd, je bent aangekomen op je eerste punt!<li>
-        <li>CODE: (Z)</li>
-        </ul>",
-        "img" : "https://lh3.googleusercontent.com/-JY9qJhQeLFc/AAAAAAAAAAI/AAAAAAAAAA0/QEUzphMhs8g/photo.jpg"
+        "content" : [
+            "Gefeliciteerd, je bent aangekomen op je eerste punt!",
+            "CODE: Ik verstop mij graag achter een hoog bord"
+        ],
+        "img" : "https://lh6.googleusercontent.com/-0wMrAGGKcZo/UOG16lRu-UI/AAAAAAAATgM/2A51mFuPNz8/s912/IMG_4581.JPG"
 
     });
+}
+
+setupFinal = function() {
+    clearLocations();
+
+    Points.insert({
+        "name":"The Final!",
+        "location":{
+            "latitude": 51.409491, 
+            "longitude": 6.041123
+        },
+        "content" : [
+        "TODO: Bel aan op nummertje 19!"
+        ],
+        "img" : "http://www.allvectors.com/wp-content/uploads/2012/02/puzzle-vector.jpg" 
+    });
+}
+
+setupLocations = function() {
+    clearLocations();
 
     Points.insert({
         "name":"Metieske",
@@ -109,11 +131,11 @@ setupLocations = function() {
             "latitude":51.412484,
             "longitude":6.035975
         },
-        "content" : "<ul>
-        <li>Foto: Maak een foto waarbij door de groep en eventueel door figuranten, uitgebeeld wordt dat het nederlands elftal in de 93e minuut van de titel 'Wereldkampioen' beroofd wordt.</li>
-        <li>Foto: Maak een foto waarin uitgebeeld wordt dat Nederland in de 93e minuut van de finale wereldkampioen wordt.</li>
-        <li>CODE: (O)</li>
-        </ul>",
+        "content" : [
+        "Foto: Maak een foto waarbij door de groep en eventueel door figuranten, uitgebeeld wordt dat het nederlands elftal in de 93e minuut van de titel 'Wereldkampioen' beroofd wordt.",
+        "Foto: Maak een foto waarin uitgebeeld wordt dat Nederland in de 93e minuut van de finale wereldkampioen wordt.",
+        "CODE: Ik ben binnen een pilsje aan het drinken",
+        ],
         "img" : "https://lh3.googleusercontent.com/-JY9qJhQeLFc/AAAAAAAAAAI/AAAAAAAAAA0/QEUzphMhs8g/photo.jpg"
 
     });
@@ -124,12 +146,12 @@ setupLocations = function() {
             "latitude":51.416472,
             "longitude":6.038467
         },
-        "content" : "<ul>
-        <li>TODO: In het begin van de vorige eeuw werd het haar van jonge dames vaak verfraaid met een bloemenkrans. Gebruik je fantasie en maak een prachtige haardecoratie met hetgeen de natuur in het blakterbeekpark je te bieden heeft. De decoratie dient tijdens de tocht door een van de groepsleden gedragen te worden. De decoratie wordt aansluitend aan de organisatie aangeboden.</li>
-        <li>Foto: Maak een foto van ludieke foto waarbij iemand deze mooie bloemenkrans draagt!</li>
-        <li>Vraag: Achterhaal wat voor bijensoort vliegt op het Blakterbeekpark en welk schapensoort er rondloopt</li>
-        <li>CODE: (NN)</li>
-        </ul>",
+        "content" : [
+        "TODO: In het begin van de vorige eeuw werd het haar van jonge dames vaak verfraaid met een bloemenkrans. Gebruik je fantasie en maak een prachtige haardecoratie met hetgeen de natuur in het blakterbeekpark je te bieden heeft. De decoratie dient tijdens de tocht door een van de groepsleden gedragen te worden. De decoratie wordt aansluitend aan de organisatie aangeboden.",
+        "Foto: Maak een ludieke foto waarbij iemand deze mooie bloemenkrans draagt!",
+        "Vraag: Achterhaal wat voor bijensoort vliegt op het Blakterbeekpark en welk schapensoort er rondloopt",
+        "CODE: Ik ben aan het schuilen; hoog en droog"
+        ],
         "img" : "http://www.depoortpijnacker.nl/wp-content/gallery/divers_1/bijen.jpg"
     });
 
@@ -139,23 +161,11 @@ setupLocations = function() {
             "latitude":51.415214,
             "longitude":6.031944
         },
-        "content" : "<ul>
-        <li>TODO: Rondje om de tafel: Welk team houdt het langste de bal in het spel?? Meeste aantal slagen heeft gewonnen.</li>
-        <li>CODE: (19)</li>
-        </ul>",
+        "content" : [
+        "TODO: Rondje om de tafel: Welk team houdt het langste de bal in het spel?? Meeste aantal slagen heeft gewonnen.",
+        "CODE: Ik vind het gezellig in mijn speelpaleisje!"
+        ],
         "img" : "http://www.startpagina.nl/athene/dochters/tafeltennis/images/tafeltennis.jpg"
-    });
-
-    Points.insert({
-        "name":"The Final!",
-        "location":{
-            "latitude":51.408566,
-            "longitude":6.040307
-        },
-        "content" : "<ul>
-        <li>TODO: Bel aan op nummertje 19!</li>
-        </ul>",
-        "img" : "http://www.allvectors.com/wp-content/uploads/2012/02/puzzle-vector.jpg" 
     });
 
     Points.insert({
@@ -164,10 +174,10 @@ setupLocations = function() {
             "latitude":51.40716,
             "longitude":6.035769
         },
-        "content" : "<ul>
-        <li>Raadsel: Het heeft een been, maar het kan niet lopen. Het heeft twee vleugels, maar het kan niet vliegen. Het draagt een bril, maar het kan niet zien. Het heeft haar, maar het kan niet gekamd worden?</li>
-        <li>CODE: (E)</li>
-        </ul>",
+        "content" : [
+        "Raadsel: Het heeft een been, maar het kan niet lopen. Het heeft twee vleugels, maar het kan niet vliegen. Het draagt een bril, maar het kan niet zien. Het heeft haar, maar het kan niet gekamd worden?",
+        "CODE: Onder een tafeltje zult u mij vinden!"
+        ],
         "img" : "http://www.animaatjes.nl/plaatjes/d/dombo/dombo10.gif"
     });
 
@@ -177,10 +187,11 @@ setupLocations = function() {
             "latitude":51.408663,
             "longitude":6.02909
         },
-        "content" : "<ul>
-        <li>Foto: Maak een foto van een zo groot mogelijke menselijke piramide!</li>
-        <li>CODE: (D)</li>
-        </ul>"
+        "content" : [
+        "Foto: Maak een foto van een zo groot mogelijke menselijke piramide!",
+        "CODE: Ik kijk graag op mensen neer"
+        ],
+        "img" : "http://img.geocaching.com/cache/4acdf6c4-df56-419e-86f8-fd6dcb215c7c.jpg?rnd=0.7269382"
     });
 
     Points.insert({
@@ -189,10 +200,10 @@ setupLocations = function() {
             "latitude":51.416756,
             "longitude":6.028275
         },
-        "content" : "<ul>
-        <li>TODO: Maak en schrijf op: De opsomming van geboortedata van opa, oma, kinderen en kleinkinderen Appeldoorn, op leeftijd van groot naar klein. (kalde kant laten we even buiten beschouwing)</li>
-        <li>CODE: (AU)</li>
-        </ul>",
+        "content" : [
+        "TODO: Maak en schrijf op: De opsomming van geboortedata van opa, oma, kinderen en kleinkinderen Appeldoorn, op leeftijd van groot naar klein. (kalde kant laten we even buiten beschouwing)",
+        "CODE: Slijmerig, maar smakelijk. Hakuna Matata!"
+        ],
         "img" : "http://www.jandurkspolder.nl/ooievaar%20JDP.JPG"
     });
 
@@ -202,17 +213,17 @@ setupLocations = function() {
             "latitude":51.409452,
             "longitude":6.036267
         },
-        "content" : "<ul>
-        <li>Vraag: Welk gebouw stond er vroeger op de plek waar nu 'ut Valdere' is. In welk jaar werd dit gebouw gesloopt en wie zat er boven op het dak om het gebouw voor sloop te behoeden.</li>
-        <li>CODE: (W)</li>
-        </ul>",
+        "content" : [
+        "Vraag: Welk gebouw stond er vroeger op de plek waar nu 'ut Valdere' is. In welk jaar werd dit gebouw gesloopt en wie zat er boven op het dak om het gebouw voor sloop te behoeden.",
+        "CODE: Het is geen Jan Wolkers"
+        ],
         "img" : "http://1.nieuwsbladcdn.be/Assets/Images_Upload/2012/08/29/A2_GOJ3U1KBC010A2_bejaarden01.jpg.h380.jpg"
     });
 }
 
 // * * * * * * * * Application Behavior * * * * * ** * * ** 
 
-DISTANCE_THRESHOLD = 10;
+DISTANCE_THRESHOLD = 15;
 
 function checkIfUserIsNearPoint() {
     var lat = gmaps.geoPosition.position.k;
@@ -230,9 +241,9 @@ function checkIfUserIsNearPoint() {
         } 
     });
     if (Session.get('current_point'))
-	$("#point").show("modal");
+	   $("#point").show("modal");
     else
-        $("#point").hide("modal");
+       $("#point").hide("modal");
 }
 
 // * * * * * * * * HELPER FUNCTIONS * * * * * ** * * ** 
