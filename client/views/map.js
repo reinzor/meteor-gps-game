3c4d7c395862b4c06a2cdb13feaecc94c0242487
Rein Appeldoorn <reinzor@gmail.com>
2014-06-25 20:24:18 +0200

    added pnts

--- client/views/map.js
+++ client/views/map.js
@@ -90,81 +90,129 @@ clearLocations = function() {
 
 setupLocations = function() {
     Points.insert({
-        "name":"metieske",
+        "name":"Speeltuintje Kuiper",
+        "location":{
+            "latitude": 51.412034,
+            "longitude": 6.032487
+        },
+        "content" : "<ul>
+        <li>Gefeliciteerd, je bent aangekomen op je eerste punt!<li>
+        <li>CODE: (Z)</li>
+        </ul>",
+        "img" : "https://lh3.googleusercontent.com/-JY9qJhQeLFc/AAAAAAAAAAI/AAAAAAAAAA0/QEUzphMhs8g/photo.jpg"
+
+    });
+
+    Points.insert({
+        "name":"Metieske",
         "location":{
             "latitude":51.412484,
             "longitude":6.035975
         },
-        "content" : "Welkom op de website van Cafe Metieske en Café Croes Moeke. Twee gezellige uitgaansgelegenheden in het centrum van Sevenum. Voor een gezellig avondje op stap of een zomeravond borrelen op het terras ben je bij ons aan het goede adres. Ook voor een verjaardagsfeest, barbecue of bedrijfsborrel bieden we vele mogelijkheden. Heb je net een leuk feestje gehad, kijk dan bij de foto’s of jouw partyshot erbij staat! Kijk snel verder voor meer informatie, het laatste nieuws, feestagenda’s en openingstijden"
+        "content" : "<ul>
+        <li>Foto: Maak een foto waarbij door de groep en eventueel door figuranten, uitgebeeld wordt dat het nederlands elftal in de 93e minuut van de titel 'Wereldkampioen' beroofd wordt.</li>
+        <li>Foto: Maak een foto waarin uitgebeeld wordt dat Nederland in de 93e minuut van de finale wereldkampioen wordt.</li>
+        <li>CODE: (O)</li>
+        </ul>",
+        "img" : "https://lh3.googleusercontent.com/-JY9qJhQeLFc/AAAAAAAAAAI/AAAAAAAAAA0/QEUzphMhs8g/photo.jpg"
+
     });
 
     Points.insert({
-        "name":"bijen",
+        "name":"Bijen",
         "location":{
             "latitude":51.416472,
             "longitude":6.038467
         },
-        "content" : "De superfamilie van de bijen (Apoidea) zijn insecten die behoren tot de orde van de vliesvleugeligen (Hymenoptera). Bijen zijn vooral bekend om de honing die ze maken. Bijen verschillen van de meeste andere (vleesetende) vliesvleugeligen door het dieet van nectar en stuifmeel. Ook de larven leven hiervan. Biologisch gezien vormen de bijen echter géén aparte groep. Alle soorten bijen behoren tot de superfamilie Apoidea, waartoe ook alle graafwespen behoren."
+        "content" : "<ul>
+        <li>TODO: In het begin van de vorige eeuw werd het haar van jonge dames vaak verfraaid met een bloemenkrans. Gebruik je fantasie en maak een prachtige haardecoratie met hetgeen de natuur in het blakterbeekpark je te bieden heeft. De decoratie dient tijdens de tocht door een van de groepsleden gedragen te worden. De decoratie wordt aansluitend aan de organisatie aangeboden.</li>
+        <li>Foto: Maak een foto van ludieke foto waarbij iemand deze mooie bloemenkrans draagt!</li>
+        <li>Vraag: Achterhaal wat voor bijensoort vliegt op het Blakterbeekpark en welk schapensoort er rondloopt</li>
+        <li>CODE: (NN)</li>
+        </ul>",
+        "img" : "http://www.depoortpijnacker.nl/wp-content/gallery/divers_1/bijen.jpg"
     });
 
     Points.insert({
-        "name":"molenberg",
+        "name":"Molenberg",
         "location":{
             "latitude":51.415214,
             "longitude":6.031944
         },
-        "content" : "Tot 1955 was er in de gemeente Delfzijl geen grote zaal met een podiumaccommodatie. In 1955 kocht de gemeente de voormalige gereformeerde kerk aan de Oude Schans. Deze kerk werd omgebouwd tot schouwburg en hier vonden tot 1973 de grotere culturele activiteiten plaats (zie foto)."
+        "content" : "<ul>
+        <li>TODO: Rondje om de tafel: Welk team houdt het langste de bal in het spel?? Meeste aantal slagen heeft gewonnen.</li>
+        <li>CODE: (19)</li>
+        </ul>",
+        "img" : "http://www.startpagina.nl/athene/dochters/tafeltennis/images/tafeltennis.jpg"
     });
 
     Points.insert({
-        "name":"karlijn",
+        "name":"The Final!",
         "location":{
             "latitude":51.408566,
             "longitude":6.040307
         },
-        "content" : "Hello everyone and welcome to my YouTube channel My name is Karlijn Verhagen and I am a 17 year old singer/songwriter from near Rotterdam in the"
+        "content" : "<ul>
+        <li>TODO: Bel aan op nummertje 19!</li>
+        </ul>",
+        "img" : "http://www.allvectors.com/wp-content/uploads/2012/02/puzzle-vector.jpg" 
     });
 
     Points.insert({
-        "name":"luttel",
+        "name":"Luttel speeltuin",
         "location":{
             "latitude":51.40716,
             "longitude":6.035769
         },
-        "content" : "gering, weinig"
+        "content" : "<ul>
+        <li>Raadsel: Het heeft een been, maar het kan niet lopen. Het heeft twee vleugels, maar het kan niet vliegen. Het draagt een bril, maar het kan niet zien. Het heeft haar, maar het kan niet gekamd worden?</li>
+        <li>CODE: (E)</li>
+        </ul>",
+        "img" : "http://www.animaatjes.nl/plaatjes/d/dombo/dombo10.gif"
     });
 
     Points.insert({
-        "name":"vlaggenpunt",
+        "name":"Vlaggenpunt",
         "location":{
             "latitude":51.408663,
             "longitude":6.02909
         },
-        "content" : "Houten vlaggetjes Foto Verjaardag, geboorte, bruiloft...of gewoon voor de leuk... Je kunt naar hartenlust je eigen vlaggenlijn samenstellen. Afmeting vlaggenpunt 12x15 cm. Prijs per vlaggenpunt €1,00 plus eenmalig maakkosten a €0,95. Ook verkrijgbaar in XL maat 16x18 cm. Prijs per vlaggenpunt €2,00 plus eenmalig maakkosten a €0,95."
+        "content" : "<ul>
+        <li>Foto: Maak een foto van een zo groot mogelijke menselijke piramide!</li>
+        <li>CODE: (D)</li>
+        </ul>"
     });
 
     Points.insert({
-        "name":"ooienvaar",
+        "name":"Ooienvaar",
         "location":{
             "latitude":51.416756,
             "longitude":6.028275
         },
-        "content" : "De ooievaar (Ciconia ciconia) is een grote vogel uit de familie ooievaars (Ciconiidae) uit de orde van de ooievaarachtigen (Ciconiiformes)."
+        "content" : "<ul>
+        <li>TODO: Maak en schrijf op: De opsomming van geboortedata van opa, oma, kinderen en kleinkinderen Appeldoorn, op leeftijd van groot naar klein. (kalde kant laten we even buiten beschouwing)</li>
+        <li>CODE: (AU)</li>
+        </ul>",
+        "img" : "http://www.jandurkspolder.nl/ooievaar%20JDP.JPG"
     });
 
     Points.insert({
-        "name":"bejaarden",
+        "name":"Bejaarden",
         "location":{
             "latitude":51.409452,
             "longitude":6.036267
         },
-        "content" : "Iemand van gevorderde leeftijd wordt wel een bejaarde genoemd. Wat die leeftijd is, is tamelijk subjectief. Vaak wordt iemand van boven de 65 als bejaard gezien, alhoewel velen het hier ook weer niet mee eens zijn."
+        "content" : "<ul>
+        <li>Vraag: Welk gebouw stond er vroeger op de plek waar nu 'ut Valdere' is. In welk jaar werd dit gebouw gesloopt en wie zat er boven op het dak om het gebouw voor sloop te behoeden.</li>
+        <li>CODE: (W)</li>
+        </ul>",
+        "img" : "http://1.nieuwsbladcdn.be/Assets/Images_Upload/2012/08/29/A2_GOJ3U1KBC010A2_bejaarden01.jpg.h380.jpg"
     });
 }
 
 // * * * * * * * * Application Behavior * * * * * ** * * ** 
 
-DISTANCE_THRESHOLD = 5;
+DISTANCE_THRESHOLD = 10;
 
 function checkIfUserIsNearPoint() {
     var lat = gmaps.geoPosition.position.k;
